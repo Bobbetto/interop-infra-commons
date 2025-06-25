d9e025b51a0a2d4af26ae9e6f97f74c5059dc5db
Bobbetto <una_parola@hotmail.com>
2025-06-25 15:37:52 +0200

    test1

--- scripts/helm/helmTemplate-svc-single.sh
+++ scripts/helm/helmTemplate-svc-single.sh
@@ -120,7 +120,7 @@ if [[ -z $microservice || $microservice == "" ]]; then
   help
 fi
 if [[ $skip_dep == false ]]; then
-  bash "$SCRIPTS_FOLDER"/helmDep.sh --untar --verbose
+  bash $SCRIPTS_FOLDER/helmDep.sh --untar --verbose
 fi
 
 VALID_CONFIG=$(isMicroserviceEnvConfigValid $microservice $environment)
