0d9368fdef7176a04b09339fcfbeb172a5ab0cdd
Bobbetto <una_parola@hotmail.com>
2025-06-26 16:22:42 +0200

    test6

--- scripts/helm/helmDep.sh
+++ scripts/helm/helmDep.sh
@@ -104,12 +104,14 @@ function setupHelmDeps() {
             echo "Processing $filename"
 
             basename_file=$(basename "$filename" .tgz)
-            chart_name="${basename_file%-*}"
+            chart_name="${basename_file}"
+            #chart_name="${basename_file%-*}"
             target_dir="charts/$chart_name"
 
             echo "→ Extracting to $target_dir"
             mkdir -p "$target_dir"
             tar -xzf "$filename" -C "$target_dir" --strip-components=1
+            rm -f "$filename"
         done    
     fi
 
